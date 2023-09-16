6c14856d5d6b73bb704374474d941fa6f870ba1f
KING BOOSKA <marcusta085@gmail.com>
2023-09-16 16:22:26 +0200

    initiation php

---
+++ type-de-donnees.php
@@ -0,0 +1,17 @@
+<?php 
+$entier = 42;
+$flottant = 3.14;
+$chaine = "Hello, world !";
+$tableau = [1, 2, 3];
+$booleen = true;
+
+// Echo permet d'afficher une value de sortir dans la console 
+echo " voici un exemple d'entier: $entier \n";
+echo " voici un exemple de flottant: $flottant \n";
+echo " voici un exemple de chaine: $chaine \n";
+echo " voici un exemple de booleen: $booleen \n";
+echo $tableau[0];
+// Tableau 
+$presentation = ["jean", "atta", "anselme", "bondoukou", "zanean", "77596686", "ingenieur", "logiciel", 42 ];
+echo "  Je suis $presentation[1] $presentation[0] $presentation[2], je reside à $presentation[3] Precisement au $presentation[4], je suis $presentation[6] $presentation[7] et je suis un jeune de $presentation[8] ans numero $presentation[5]";
+?>
---
+++ variable.php
@@ -0,0 +1,6 @@
+<?php 
+$nom = "john";
+$âge = 30;
+echo " je m'appel $nom \n ";
+echo " j'ai $âge";
+?>
